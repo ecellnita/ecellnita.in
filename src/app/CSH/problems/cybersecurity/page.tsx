e42aca4f9f0e65fd44dddfa356c034d60899ba25
crampussm <swaparup36@gmail.com>
2024-11-05 17:43:13 +0530

    website build fixed

---
+++ src/app/CSH/problems/cybersecurity/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+        <div className='relative w-full bg-cover bg-center overflow-hidden bg-[#0D0628]'>
+            <div className="relative py-16 bg-cover bg-center overflow-hidden bg-[#0D0628] bg-[url('/photos/ieif/ieif-bg.png')]">
+                <div className='mt-10 w-[85%]'>
+                    <h1 className='text-5xl font-bold text-white'>Cyber Security Hackathon</h1>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default page
