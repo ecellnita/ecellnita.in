80861f5a0cd25b47b6f4e3ce84364d2f5cf12d12
ecellnita <ecellnita@gmail.com>
2024-11-05 18:26:18 +0530

    prod fixes

--- src/app/CSH/problems/cybersecurity/page.tsx
+++ src/app/CSH/problems/cybersecurity/page.tsx
@@ -6,7 +6,10 @@ function page() {
         <div className='relative w-full bg-cover bg-center overflow-hidden bg-[#0D0628]'>
             <div className="relative py-16 bg-cover bg-center overflow-hidden bg-[#0D0628] bg-[url('/photos/ieif/ieif-bg.png')]">
                 <div className='mt-10 w-[85%]'>
-                    <h1 className='text-5xl font-bold text-white'>Cyber Security Hackathon</h1>
+                  <h1 className='text-5xl font-bold text-white'>Cyber Security Hackathon</h1>
+                </div>
+                <div>
+                  <h2 className='text-white font-semibold text-3xl'>PS-2</h2>
                 </div>
             </div>
         </div>
